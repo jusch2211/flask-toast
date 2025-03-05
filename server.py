453890482b0c31f7801c276a8bf055b2777ce0c1
jusch <julian@scheichel.net>
2025-03-05 09:12:30 +0100

    Mit CSS-Lib

--- server.py
+++ server.py
@@ -14,4 +14,4 @@ def trigger_toast():
     return redirect(url_for('index'))  # Weiterleitung zur Startseite
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
